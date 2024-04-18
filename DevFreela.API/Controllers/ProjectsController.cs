9525533523acd360c7116110e68ce14d17944430
Aline Diani <alinediani99@gmail.com>
2024-04-17 22:37:04 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,45 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/projects")]
+    public class ProjectsController : Controller
+    {
+        
+        [HttpGet("/get")]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel model)
+        {
+            if(model.Title.Length > 50) 
+            {
+                return BadRequest(model.Title);
+            }
+            return CreatedAtAction(nameof(GetById), new { id = model.Id },  model);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,[FromBody] UpdateProjectModel model)
+        {
+            if (model.Description.Length > 50)
+            {
+                return BadRequest(model.Description);
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id, [FromBody] UpdateProjectModel model)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [ApiController]
+    [Route("api/skills")]
+    public class SkillsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
